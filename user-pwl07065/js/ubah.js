65400fbc41da652cf284a38977baa61c2ba0cb70
satria07065 <hokieklw321@gmail.com>
2025-04-17 22:21:55 +0700

    Delete user-pwl07065 directory

--- user-pwl07065/js/ubah.js
+++
@@ -1,5 +0,0 @@
-const ubah=document.getElementById('judul');
-ubah.style.color='red';
-ubah.style.backgroundColor='green';
-//judul1.innerHTML=‘Welcome UDINUS';
-
--- user-pwl07065/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_unset(); 
-session_destroy();
-session_regenerate_id(true); // Regenerasi ID session untuk keamanan
-header("location: index.php"); // Kembali ke halaman login
-exit;
-?>
